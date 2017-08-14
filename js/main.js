19dda30ffb20e2a1e0a905d9267d946081f0bb8a
Andrew Brown <andrewtcb@gmail.com>
2017-08-14 23:21:10 +1000

    fixed padding

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+$(document).ready(function() {
 
+  $(".container").animate({opacity: '1'}, 'slow');
+
+  //Animate arrow
+  $('#arrow').on('mouseenter', function(){
+    $("#arrow").animate({opacity: '0.3'}, 'medium');
+  })
+  $('#arrow').on('mouseleave', function(){
+    $("#arrow").animate({opacity: '1'}, 'medium');
+  })
+
+  //Animate menu
+  $('.nav').on('mouseenter', function(){
+    $(this).animate({opacity: '0.3'}, 'fast');
+  })
+  $('.nav').on('mouseleave', function(){
+    $(this).animate({opacity: '1'}, 'fast');
+  })
+
+  //Animate scroll on arrow click
+  $("#arrow").on('click', function(event) {
+    $('html, body').animate({
+      scrollTop: $('#about').offset().top
+    }, 800)
+  })
+})

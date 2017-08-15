461873b6a5f02abe12f5ed4957865c15afc65650
Andrew Brown <andrewtcb@gmail.com>
2017-08-15 10:16:48 +1000

    added resume

--- js/main.js
+++ js/main.js
@@ -18,10 +18,28 @@ $(document).ready(function() {
     $(this).animate({opacity: '1'}, 'fast');
   })
 
+  $('.button').on('mouseenter', function(){
+    $(this).animate({opacity: '0.3'}, 'fast');
+  })
+  $('.button').on('mouseleave', function(){
+    $(this).animate({opacity: '1'}, 'fast');
+  })
+
   //Animate scroll on arrow click
-  $("#arrow").on('click', function(event) {
+  $("#arrow").on('click', function() {
     $('html, body').animate({
-      scrollTop: $('#about').offset().top
+      scrollTop: $('#skills').offset().top
     }, 800)
   })
+
+  $("#menu").on("scroll", function() {
+
+  if (this.scrollTop > 347) {
+    console.log('hey')
+    $("#menu").addClass("fix-menu");
+  } else {
+    $("#menu").removeClass("fix-menu");
+  }
+
+});
 })
